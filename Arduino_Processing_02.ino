2d8091eed754a51d5b047841f056b78cac5d78c1
Felipe Nascimento Martins <felipe.n.martins@gmail.com>
2018-06-04 11:57:58 +0200

    Add files via upload

---
+++ Arduino_Processing_02.ino
@@ -0,0 +1,31 @@
+char val;
+int ledPin = 5;
+boolean ledState = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  establishContact();
+}
+
+void loop() {
+  if (Serial.available() > 0){
+    val = Serial.read();
+    if (val == '1'){
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+    }
+    delay(10);
+  }
+  else{
+    Serial.println(analogRead(A0));
+    delay(50);
+  }
+}
+
+void establishContact(){
+  while (Serial.available() <=0){
+    Serial.println('A');
+    delay(300);
+  }
+}
